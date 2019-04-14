fb7613c15f7e4d59206319fca2f63bb49044ad83
Amit Gaikwad <amitgaikwad85@gmail.com>
2019-04-14 14:34:24 +0530

    initial commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,13 +4,21 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faAward } from '@fortawesome/free-solid-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
 import { HomePage } from './home.page';
+
+library.add(fas, faAward);
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    FontAwesomeModule,
     RouterModule.forChild([
       {
         path: '',
